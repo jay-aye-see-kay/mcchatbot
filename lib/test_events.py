32b7a90bf0b187ddecc4eb280e9fc13d53a37f46
Jack Rose <user@jackrose.co.nz>
2023-09-25 21:37:37 +1000

    break events off into own module

---
+++ lib/test_events.py
@@ -0,0 +1,32 @@
+import unittest
+
+from lib.events import is_death
+
+is_death_ignorable_logs = [
+    "[player_XX: Set own game mode to Spectator Mode]",
+    "Thread [RCON](2023-09-24_rcon.md) Client /127.0.0.1 started",
+    "Thread RCON Client /127.0.0.1 shutting down",
+    "player_XX lost connection: Disconnected",
+    "player_XX left the game",
+    "UUID of player Player_YY is 548724a6-ae80-41af-9144-f6a02ab27fcb",
+    "Player_YY joined the game",
+    "Player_YY[/172.19.0.4:60669] logged in with entity id 53844 at ([world]210.27267354636194, 167.5, -97.29571541611286)",  # noqa: E501
+    "Villager EntityVillager['Farmer'/61310, uuid='a0772acd-e86c-44f5-ad3a-b21c26ad0a7e', l='ServerLevel[world]', x=220.30, y=179.00, z=-63.30, cpos=[13, -4], tl=395686, v=true] died, message: 'Farmer was slain by Zombie'",  # noqa: E501
+]
+
+
+class TestIsDeath(unittest.TestCase):
+    def test_ignores_villager(self):
+        self.assertFalse(is_death("Villager blew up"))
+
+    def test_ignores_other_logs(self):
+        for log in is_death_ignorable_logs:
+            with self.subTest(log=log):
+                self.assertFalse(is_death(log))
+
+    def test_true_on_death(self):
+        self.assertTrue(is_death("Player_YY blew up"))
+
+
+if __name__ == "__main__":
+    unittest.main()
