4f4f11f57845321ed6f8ad6b10dbbf954b74e10e
Jack Rose <user@jackrose.co.nz>
2023-09-30 20:53:10 +1000

    fix: commit after insert is required

--- lib/db.py
+++ lib/db.py
@@ -1,3 +1,4 @@
+import logging
 import sqlite3
 
 from lib.config import Config
@@ -5,17 +6,21 @@ from lib.events import LogEvent
 
 
 def init_db(cfg: Config) -> sqlite3.Connection:
+    logging.info(f"opening sqlite file: {cfg.db_path}")
     con = sqlite3.connect(cfg.db_path)
     con.row_factory = sqlite3.Row
     return con
 
 
 def ensure_db_setup(db: sqlite3.Connection):
+    logging.debug('ensuring db table "events" exists')
     db.execute("CREATE TABLE IF NOT EXISTS events(event_type, time, username, text)")
 
 
 def save_event(db: sqlite3.Connection, event: LogEvent):
+    logging.debug(f"inserting {tuple(event)} into events")
     db.execute("INSERT INTO events VALUES (?, ?, ?, ?)", tuple(event))
+    db.commit()
 
 
 def query_context_messages(cfg: Config, db: sqlite3.Connection):
